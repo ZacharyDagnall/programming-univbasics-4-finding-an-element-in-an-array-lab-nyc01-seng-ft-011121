2556994d947ee004fd5c487d286e4fced2929f4d
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-03 23:22:06 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  index = -1
+  count = 0 
+  while array[count] do
+    if(array[count]==value_to_find) do
+      index = count
+      break
+    else count+=1 
+  end
+  if(index==-1) do
+    return nil 
+  else return index
+  end 
 end
